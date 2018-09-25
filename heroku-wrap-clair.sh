e742c603bef4b78f0f5488df1a89bb7aff30c73b
William Schmitt <w.schmitt@salesforce.com>
2018-09-24 18:52:46 -0700

    iii

--- heroku-wrap-clair.sh
+++ heroku-wrap-clair.sh
@@ -2,4 +2,4 @@
 # heroku-ify the clair config
 python herokuify.py /etc/clair/config.raw.yaml $1 > /etc/clair/config.yaml
 # run clair normally
-dumb-init -- /clair -config=$HOME/clair/config.yaml
+dumb-init -- /clair -config=/clair/config.yaml
